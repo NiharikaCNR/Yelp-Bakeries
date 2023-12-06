ee1a1973f95764aa1758eee25a6598a2c5db61cf
NiharikaCNR <niharikacnr99@gmail.com>
2023-12-05 22:39:27 -0600

    Update app

--- app.R
+++ app.R
@@ -14,27 +14,28 @@ library(bslib)
 library(shinydashboard)
 library(leaflet)
 library(shinyWidgets)
+library(fmsb)
 
 theme_set(theme_classic())
 
-tmap_mode("view")
+tmap_mode('view')
 options(tigris_use_cache = TRUE)
 
 # Read Files
-bakeries = read_csv("https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/bakeries.csv") %>% 
+bakeries = read_csv('https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/bakeries.csv') %>% 
   mutate(is_open = factor(is_open, levels = c(0,1)), price_range=if_else(is.na(price_range), 0, as.numeric(price_range))) %>% 
-  mutate(dollar_signs = strrep("$",price_range))
-  
+  mutate(dollar_signs = strrep('$',price_range))
 bakery_reviews = read.csv(file = 'https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/bakery_reviews.csv')
-incomes <- read_csv("https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/ca-sb-mean-incomes.csv")
-sentiment_scores <- read_csv("https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/sentiment_scores.csv")
+aspect_scores_overall <- read_csv('https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/aspect_scores_overall.csv')
+incomes <- read_csv('https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/ca-sb-mean-incomes.csv')
+sentiment_scores <- read_csv('https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/sentiment_scores.csv')
+valid_bakeries <- read.table('https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/data/valid_bakeries.txt', header = FALSE)$V1
 
 # Create plot data
 ## Bakeries map data
 zipcodes <- incomes$zip
-valid_bakeries <- bakeries %>% filter(postal_code %in% zipcodes) #%>% pull(business_id)
-open_bakeries = valid_bakeries %>% filter(is_open==1)
-closed_bakeries = valid_bakeries %>% filter(is_open==0)
+open_bakeries = bakeries %>% filter(business_id %in% valid_bakeries & is_open==1)
+closed_bakeries = bakeries %>% filter(business_id %in% valid_bakeries & is_open==0)
 
 bakeries_summary = bakery_reviews %>% group_by(business_id) %>% 
   summarise(num_reviews = n(), avg_rating = sum(stars)/num_reviews, first_review = min(date), last_review = max(date),
@@ -44,14 +45,17 @@ bakeries_summary = bakery_reviews %>% group_by(business_id) %>%
 
 y <- 2022
 
-bakeries_map_data <- inner_join(valid_bakeries, bakeries_summary, by='business_id') %>% 
+bakeries_map_data <- inner_join(
+  bakeries %>% filter(business_id %in% valid_bakeries), 
+  bakeries_summary, by='business_id'
+  ) %>% 
   filter(num_reviews > 50) %>% 
-  inner_join(sentiment_scores) %>% 
+  inner_join(sentiment_scores) %>%
   select(latitude, longitude, name, business_id, is_open, avg_rating, opening_year, closing_year, sentiment_label, dollar_signs) %>% 
-  st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+  st_as_sf(coords = c('longitude', 'latitude'), crs = 4326)
 
 ## Zip code Income Map data
-zipcode_boundaries <- zctas(state="CA", year=2010) %>% 
+zipcode_boundaries <- zctas(state='CA', year=2010) %>% 
   filter(ZCTA5CE10 %in% zipcodes) %>% 
   select(zip=ZCTA5CE10, lat=INTPTLAT10, long=INTPTLON10, geometry) %>% 
   mutate(zip=as.numeric(zip), lat=as.numeric(lat), long=as.numeric(long), geometry)
@@ -60,19 +64,21 @@ zipcode_map_data <- incomes %>%
   inner_join(zipcode_boundaries) %>% 
   st_as_sf(sf_column_name='geometry')
 
-#TODO:  Maps -- REMOVE
-# bakeries_map <- tm_shape(bakeries_map_data) + 
-#   tm_dots(col = 'is_open_now', size = 'avg_rating', palette="Set1", alpha=0.7)
-#  bakeries_map
-# zipcode_income_map <- tm_shape(zipcode_map_data) +
-#   tm_polygons(col='2020', alpha = 0.7)
-#  zipcode_income_map
-
-##TODO: Final Map -- REMOVE
-# zipcode_income_map + bakeries_map + tm_view(set.view = c(-122,34.44,14), set.zoom.limits = c(14,17))
-
-# ------------------------------------------------------------------------------------------------------------------------
-
+beautiful_radarchart <- 
+  function(data, color = '#00AFBB', vlabels = colnames(data), vlcex = 0.7, caxislabels = NULL, title = NULL, ...) {
+    radarchart(
+      data, axistype = 1,
+      # Customize the polygon
+      pcol = color, pfcol = c(scales::alpha(color[1],0.5),rep(NA,4)), plwd = 2, plty = 1,
+      # Customize the grid
+      cglcol = 'grey', cglty = 1, cglwd = 0.8,
+      # Customize the axis
+      axislabcol = 'white', 
+      # Variable labels
+      vlcex = 1, vlabels = vlabels,
+      caxislabels = caxislabels, title = title, ...
+    )
+  }
 
 update_map <- function(y) {
   # Filtering data for Map-1
@@ -84,7 +90,7 @@ update_map <- function(y) {
   # Map-1: A map showing locations of open and closed bakeries in Santa Barbara, CA in year 'y'
   bakeries_map <- tm_shape(bakeries_map_data) + 
   tm_bubbles(
-    col='is_open_now', size='avg_rating', palette="Set1", alpha=0.7,scale=0.2, 
+    col='is_open_now', size='avg_rating', palette='Set1', alpha=0.7,scale=0.2, 
     popup.vars=c('Name'='name', 
                  'Average Rating:'='avg_rating', 
                  'Price Category:' = 'dollar_signs',
@@ -96,7 +102,7 @@ update_map <- function(y) {
   
   # Map-2: A map showing avg. incomes of open and closed bakeries in Santa Barbara, CA in year 'y'
   zipcode_income_map <- tm_shape(zipcode_map_data) +
-    tm_polygons(col=y, alpha = 0.6, palette="Greens", popup.vars=c('Avg. Income: '=y))
+    tm_polygons(col=y, alpha = 0.6, palette='Greens', popup.vars=c('Avg. Income: '=y))
   # tmap_leaflet(zipcode_income_map)
   
   ## Final Map: Map-1 overlayed over Map-2 
@@ -104,59 +110,177 @@ update_map <- function(y) {
   
 }
 
+update_radar_plot <- function(bakery_name, open_status) {
+  limit_rows <- aspect_scores_overall %>% 
+    head(3) %>%
+    select(-c(2,3,4))
+  
+  if(bakery_name=='All') {
+    name <- paste('All',open_status,'Bakeries Scores')
+    bakeries_list <- if(open_status=='Open') open_bakeries else closed_bakeries
+    selected_bakery <- aspect_scores_overall %>% 
+      filter(business_id %in% bakeries_list$business_id) %>% 
+      summarize_at(vars(colnames(aspect_scores_overall)[5:10]), ~round(mean(.),2), .names = '{.col}') %>% 
+      mutate(business_id = name, .before = stars)
+    bakery_name <- selected_bakery$business_id[1]
+  }
+  else {
+    selected_bakery <- aspect_scores_overall %>% 
+      filter(name==bakery_name) %>% 
+      head(1) %>% 
+      select(-c(2,3,4))
+  }
+    
+  radar_plot_data <- rbind(limit_rows,selected_bakery)
+  sample <- as.data.frame(radar_plot_data)
+  rownames(sample) <- c(sample$business_id[1:3],bakery_name)
+  sample <- sample %>% select(c('Ambiance','Drinks','Food','Price','Service'))
+  
+  # CONSTRUCTING RADAR PLOT
+  op <- par(mar = c(1, 2, 2, 2))
+  beautiful_radarchart(sample, caxislabels=c(), c('#d9d9d9', '#605ca8'))
+  legend(
+    x = 'bottom', legend = rownames(sample[-c(1,2),]), horiz = T,
+    bty = 'n', pch = 20 , col = c('#d9d9d9', '#605ca8'),
+    text.col = 'black', cex = 1, pt.cex = 1.75
+  )
+  # par(op)
+}
+
+generate_summary <- function(bakery_name) {
+  if (bakery_name=="All") {
+    return (paste0('<h3>Please select a bakery for displaying summary</h3>'))
+  }
+  # bakery_name <- 'Helena Avenue Bakery'
+  avg <- aspect_scores_overall[3,]
+  selected <- aspect_scores_overall %>% filter(name==bakery_name) %>% head(1)
+  
+  amb_text <- "Select a bakery to get summary"
+  if((avg$Ambiance-selected$Ambiance)>0) {
+    amb_text <- "You need to make imporovements to your bakery ambiance."
+  }else {
+    amb_text <- "You are already outdoing other bakeries. Keep it up!"
+  }
+  
+  drinks_text <- "Select a bakery to get summary"
+  if((avg$Drinks-selected$Drinks)>0) {
+    drinks_text <- "You need to updates the quality and menu of the drinks you offer."
+  }else {
+    drinks_text <- "You are already outdoing other bakeries. Keep it up!"
+  }
+  
+  food_text <- "Select a bakery to get summary"
+  if((avg$Food-selected$Food)>0) {
+    food_text <- "You need to revisit the quality of your food."
+  }else {
+    food_text <- "You are already outdoing other bakeries. Keep it up!"
+  }
+  
+  price_text <- "Select a bakery to get summary"
+  if((avg$Price-selected$Price)>0) {
+    price_text <- "Your menu prices are competitive and affordable. Keep it up!"
+  }else {
+    price_text <- "You need to revamp the pricing of your menu items"
+  }
+  
+  service_text <- "Select a bakery to get summary"
+  if((avg$Service-selected$Service)>0) {
+    service_text <- "Your staff and service needs to be more approachable."
+  }else {
+    service_text <- "You are already outdoing other bakeries. Keep it up!"
+  }
+  
+  tags <- paste0('<font size="2.5"><span><b><u>Ambiance Summary:</u></b></span></font>',
+                 '<font size="2"><br>',amb_text,'</font><br>',
+                 '<font size="2.5"><span><b><u>Drinks Summary:</u></b></span></font>',
+                 '<font size="2"><br>',drinks_text,'</font><br>',
+                 '<font size="2.5"><span><b><u>Food Summary:</u></b></span></font>',
+                 '<font size="2"><br>',food_text,'</font><br>',
+                 '<font size="2.5"><span><b><u>Price Summary:</u></b></span></font>',
+                 '<font size="2"><br>',price_text,'</font><br>',
+                 '<font size="2.5"><span><b><u>Service Summary:</u></b></span></font>',
+                 '<font size="2"><br>',service_text,'</font>')
+  # tags <- (
+  #   tags$p("Ambiance: "),
+  #   tags$p("Drinks: "),
+  #   tags$p("Food: "),
+  #   tags$p("Price: "),
+  #   tags$p("Service: "),
+  # )
+  # if (bakery_name == "All"){
+  return (tags)
+  # }
+}
+
 
 home_page_body <- function() {
   fluidPage(
-    style = "font-size: 2rem; text-align: justify;",
-    h1("What do we do?"),
-    p("[Insert Text here]"),
+    style = 'font-size: 2rem; text-align: justify;',
+    h1('What is this app?'),
+    p('The overall goal of this application is to provide useful, analytical insights to bakery store business owners on
+Yelp, making it easy to be understood.'),
+    p('Furthermore, we focus on this specific goal: We aim to provide advice for bakery shop owners following the key impact factors extracted from reviews and the correlation with regional income.'), 
+    p('We are going to answer: What factors contribute to the opening and close of bakery shops in Santa Barbara?'),
     tags$br(),
-    h1("How to use this site?"),
+    h1('How do I navigate?'),
     tags$ol(
-      tags$li(""),
-      tags$li(""),
-      tags$li(""),
-      tags$li(""),
+      tags$li('Map: A map of the open and closed bakeries in Santa Barbara, CA, overlaid with a choropleth map of income across zip codes. Click on each point to view more information on each bakery. Choose years to see what bakeries were open and clossed each year.'),
+      tags$li('Radar Plots: Graphs of open and closed bakeries based on five aspects of comparison'),
     )
   )
 }
 
 map_page_body <- function() {
   fluidPage(
-    prettyRadioButtons('year', "Select a year:", choices=rev(colnames(incomes)[-1]), selected='2019', inline=T),
-    leafletOutput("income_bakeries_map", height = 700),
+    prettyRadioButtons('year', 'Select a year:', choices=rev(colnames(incomes)[-1]), selected='2019', inline=T),
+    leafletOutput('income_bakeries_map', height = 700)
   )
 }
 
-sentiment_page_body <- function() {
+radar_page_body <- function() {
   fluidPage(
-    h1("Sentiment Page")
+    fluidRow(
+      sidebarLayout(
+        sidebarPanel(
+          selectizeInput( 'open_bakery', 'Select an open bakery:', c('All', unique(open_bakeries$name)) ),
+          htmlOutput('open_summary')
+        ),
+        mainPanel(
+          plotOutput('open_radar_plot', height = '375px')
+        )
+      )
+    ),
+    tags$hr(), 
+    fluidRow(
+      sidebarLayout(
+        sidebarPanel(
+          selectizeInput( 'closed_bakery', 'Select a closed bakery:', c('All', unique(closed_bakeries$name)) ),
+          htmlOutput('closed_summary')
+        ),
+        mainPanel(
+          plotOutput('closed_radar_plot', height = '375px')
+        )
+      )
+    )
   )
 }
-
 
 ui <- dashboardPage(
-  header = dashboardHeader(title = "Bakeries in Santa Barbara, CA", titleWidth = "calc(20%)"),
+  header = dashboardHeader(title = 'Bakeries in Santa Barbara, CA', titleWidth = 'calc(20%)'),
   sidebar = dashboardSidebar(
     sidebarMenu(
-      id = "tabs",
-      menuItem("Home", tabName = "home_page"),
-      menuItem("Map" , tabName = "map_page" ),
-      menuItem("Sentiment Analysis", tabName = "sentiment_page")
-      # menuItem("Reporting Tool", tabName = "reporting_tool"),
-      # menuItem("Result Summary", tabName = "result_summary") , 
-      # menuItem("Team", tabName = "team_page")
+      id = 'tabs',
+      menuItem('Home', tabName = 'home_page'),
+      menuItem('Map' , tabName = 'map_page' ),
+      menuItem('Radar Plots', tabName = 'radar_page')
       )
     ),
   body = dashboardBody( 
-    includeCSS("https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/styles/app.css"),
+    includeCSS('https://github.com/NiharikaCNR/Yelp-Bakeries/raw/main/styles/app.css'),
     tabItems( 
-      tabItem( tabName = "home_page", home_page_body() ),
-      tabItem( tabName = "map_page",  map_page_body()  ),
-      tabItem( tabName = "sentiment_page", sentiment_page_body())
-      # tabItem(tabName = "reporting_tool", reporting_tool_body()), 
-      # tabItem(tabName = "result_summary"), 
-      # tabItem(tabName = "team_page", team_tab_body())
+      tabItem( tabName = 'home_page', home_page_body() ),
+      tabItem( tabName = 'map_page',  map_page_body()  ),
+      tabItem(tabName = 'radar_page', radar_page_body())
       )
     ),
   skin = 'purple'
@@ -165,20 +289,17 @@ ui <- dashboardPage(
 
 server <- function(input, output, session) {
   y <- reactive({input$year})
-  # message(year())
+  open_bakery <- reactive({input$open_bakery})
+  closed_bakery <- reactive({input$closed_bakery})
+  
   output$selected_year <- renderText(y())
   output$income_bakeries_map <- renderLeaflet(update_map(y()))
-  # observeEvent(input$year, {
-  #   output$income_bakeries_map <- renderLeaflet({
-  #     # data("World")
-  #     # tmap_leaflet(tm_shape(World) + tm_borders())
-  #     # browser()
-  #     message("------------HELLOOOOOOO------------------")
-  #     # browser()
-  #     update_map(y())
-  #   })
-  # })
   
+  output$open_radar_plot <- renderPlot(update_radar_plot(open_bakery(),'Open'))
+  output$open_summary <- renderText(generate_summary(open_bakery()))
+  
+  output$closed_radar_plot <- renderPlot(update_radar_plot(closed_bakery(),'Closed'))
+  output$closed_summary <- renderText(generate_summary(closed_bakery()))
 }
 
 shinyApp(ui, server)
